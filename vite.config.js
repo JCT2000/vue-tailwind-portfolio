d778f4c5b64ab025c725dcc0b1df38853b88ea4b
John Christian Triana <trianajohnchristian@gmail.com>
2023-12-06 18:11:08 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,13 @@
-// vite.config.js
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-import VitePages from 'vite-plugin-pages';
-import ViteLayouts from 'vite-plugin-vue-layouts';
-import ViteWind from 'vite-plugin-windicss';
-import ViteSvgIcons from 'vite-plugin-svg-icons';
-import ViteComponents from 'vite-plugin-components';
-import ViteIconsResolver from 'vite-plugin-icons-resolver';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: "/vue-tailwind-portfolio/",
-  plugins: [vue(),
-    VitePages(),
-    ViteLayouts(),
-    ViteWind(),
-    ViteSvgIcons({
-      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
-      symbolId: 'icon-[name]',
-    }),
-    ViteComponents(),
-    ViteIconsResolver(),
-    // Add vite-plugin-gh-pages
-    require('vite-plugin-gh-pages')(),],
-  
-  assetsInclude: ["**/*.JPG"],
+  base: '/triana-portfolio/',
+  plugins: [vue()],
+  assetsInclude: ['**/*.JPG'],
   resolve: {
     alias: {
-      "@": "/src", // Adjust the alias path
+      '@': '/src', // Adjust the alias path
     },
   },
 });

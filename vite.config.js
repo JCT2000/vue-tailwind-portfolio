cd59f77b4dd4a024cc1f9765070f361ec7a8a395
John Christian Triana <trianajohnchristian@gmail.com>
2023-12-06 17:45:19 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import VitePages from 'vite-plugin-pages';
+import ViteLayouts from 'vite-plugin-vue-layouts';
+import ViteWind from 'vite-plugin-windicss';
+import ViteSvgIcons from 'vite-plugin-svg-icons';
+import ViteComponents from 'vite-plugin-components';
+import ViteIconsResolver from 'vite-plugin-icons-resolver';
+
 export default defineConfig({
   base: "/vue-tailwind-portfolio/",
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePages(),
+    ViteLayouts(),
+    ViteWind(),
+    ViteSvgIcons({
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+      symbolId: 'icon-[name]',
+    }),
+    ViteComponents(),
+    ViteIconsResolver(),
+    // Add vite-plugin-gh-pages
+    require('vite-plugin-gh-pages')(),],
+  
   assetsInclude: ["**/*.JPG"],
   resolve: {
     alias: {
